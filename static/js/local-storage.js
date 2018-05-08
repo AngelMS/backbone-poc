de8ca8e9ae8f23c5d3f318b0a5e5f2f170f16503
AngelMS <Angel.m.Some@gmail.com>
2018-05-08 09:20:14 +0200

    local-storage support

---
+++ static/js/local-storage.js
@@ -0,0 +1,17 @@
+
+
+
+
+$(function() {
+    
+    app.TodoList = Backbone.Collection.extend({
+        model: app.Todo,
+        localStorage: new Store("backbone-todo")
+    });
+    
+    
+    // instance of the Collection
+    app.todoList = new app.TodoList();
+    
+    
+});
